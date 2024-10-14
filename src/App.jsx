8028984a9e70d6658e9cf58967bfae4a38fdb206
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-14 19:07:26 -0300

    Atividade19

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Div from './components/Div'
+
+function App() {
+
+  return (
+    <>
+    <Div/>
+    <Div/>
+    <Div/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Div.jsx
@@ -0,0 +1,5 @@
+function Div(){
+    return <div style={{margin:"10px", backgroundColor:"black", color:"white", fontFamily:"monospace"}}><p>DIV COM ESTILO</p></div>
+}
+
+export default Div
